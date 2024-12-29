70acec0c4383eea9ff45d4b32f8ef8b854d9204d
MrKyosuke <123309506+MrKyosuke@users.noreply.github.com>
2024-12-30 01:36:38 +0700

    Update QuizData.kt
    changes dari yg sekarang paling untuk adjust saja bagi Easy Levelnya

--- MyKotlinApp/app/src/main/java/com/example/mykotlinapp/QuizData.kt
+++ MyKotlinApp/app/src/main/java/com/example/mykotlinapp/QuizData.kt
@@ -4,11 +4,16 @@ data class QuizData(
     val questions: List<QuizQuestion>
 )
 
-//tipe data dari tiap object di storynya
 data class QuizQuestion(
-    val imageResId: Int,
     val question: String,
-    val answers: List<String>,
-    val correctAnswerIndex: Int
+    val imageResId: Int, // Ini masih testing, tabi coba ditest ulang untuk panggil ID dari panggil image
+    val answers: List<String>, // Apparantely ini untuk manggil String tiap Quiz data dalam Medium mode, yang berarti hanya untuk tiap Text yang akan muncul dari Question yang dimasukan di Repository
+    val correctAnswerIndex: Int, // Store data answer dari sini, dalam bentuk Int karena akan store option yang dipilih User yang mana
+    val answerImageRes: List<Int>? = null // ini untuk value dari resolution image yang ingin ditampilkan
 )
+
+
+
+
+
 
